30a0bc80b0f447173b5baf45cf67f25c6e8bc42a
Franklin Liu <franklin.liu@autodata.net>
2019-11-19 10:33:25 -0500

    got graphql to working again

--- services/src/db/connect.js
+++ services/src/db/connect.js
@@ -2,5 +2,6 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 mongoose.connect("mongodb://fady:fliu253@ds335648.mlab.com:35648/manga-reader", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
